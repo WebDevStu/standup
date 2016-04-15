3f2affa198088d5eb7d334f96cfbb720545f0420
webdevstu <git@stewart-anderson.co.uk>
2016-04-15 23:32:38 +0100

    Update to readme with plan and testing commands

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # standup
+
 git log recursively on repos to see what I did yesterday (or Friday if a Monday)
+
+
+## TODO
+Get pwd and iterate sub directories looking for .git then run "git log" with params to filter to yesterday and only my user (read .gitconfig), output in pretty format
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 #!/usr/bin/env node
 
-console.log('lol');
+"use strict";
+
+var util = require('util'),
+    exec = require('child_process').exec;
+
+
+// log out the output for now
+function logger (error, stdout, stderr) {
+    console.log(stdout)
+}
+
+// git log
+exec("git log", logger);
